fc28f720570ebf3aa4729e8870abe4c35ebc300d
Sara Salzano <sarasalzano95@gmail.com>
2025-11-07 15:02:31 +0100

    import App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TravelCards from "./component/TravelCards";
 import './App.css'
 
-function App() {
-
-
+export default function App() {
   return (
-   <>
-   </> 
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TravelCards />} />
+        <Route path="/viaggi/roma" element={<h1 className="text-center mt-5"></h1>} />
+        <Route path="/viaggi/parigi" element={<h1 className="text-center mt-5"></h1>} />
+        <Route path="/viaggi/tokyo" element={<h1 className="text-center mt-5"></h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
+   
