c937393550c2918792578be9c8637ef0f484241d
Sara Salzano <sarasalzano95@gmail.com>
2025-11-07 16:23:59 +0100

    inserisci searchbar in accordion

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,4 +1,24 @@
-export default function DetailPage(){
+import { Accordion } from "react-bootstrap";
+import travellers from "../../data/travellers";
+import SearchBar from "../components/SearchBar";
 
-    return(<></>)
+export default function DetailPage() {
+  return (
+    <>
+    <SearchBar />
+    {travellers.map((t) => (
+    <Accordion key={t.id} defaultActiveKey={['0']} alwaysOpen>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>{t.nome} {t.cognome}</Accordion.Header>
+        <Accordion.Body>
+          <span>{t.email}</span> 
+          <span>{t.cellulare}</span>
+          <span>{t.codice_fiscale}</span> 
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+    ))}
+    </>
+  );
 }
+
