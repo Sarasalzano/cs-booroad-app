5df925e24133c0405c17939a8a6b887ccb1d20bb
Sara Salzano <sarasalzano95@gmail.com>
2025-11-07 17:16:47 +0100

    macrostruttura searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
 
-export default function SearchBar(){
-    return(
-    <div>
-        <input type="text" placeholder="cerca..." />
-    </div>
-    )
+
+export default function SearchBar({ onSearch }) {
+  const [query, setQuery] = useState("");
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setQuery(value);
+    onSearch(value); 
+  };
+
+  return (
+    <input
+      type="text"
+      className="form-control mb-3"
+      placeholder= "cerca"
+      value={query}
+      onChange={handleChange}
+    />
+  );
 }
+
