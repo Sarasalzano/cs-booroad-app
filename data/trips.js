d63b838b764541f804efbcb0e0d5e9b73ec6ec45
Sara Salzano <sarasalzano95@gmail.com>
2025-11-07 15:05:34 +0100

    crea file trips.js

---
+++ data/trips.js
@@ -0,0 +1,24 @@
+const trips = [
+  {
+    id: 1,
+    name: "Roma",
+    duration: "3 giorni",
+    image: "",
+    path: "/viaggi/roma",
+  },
+  {
+    id: 2,
+    name: "Parigi",
+    duration: "5 giorni",
+    image: "",
+    path: "/viaggi/parigi",
+  },
+  {
+    id: 3,
+    name: "Tokyo",
+    duration: "7 giorni",
+    image: "",
+    path: "/viaggi/tokyo",
+  },
+];
+
