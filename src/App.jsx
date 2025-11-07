54276065d66758702da04151fbae93894ffc7e32
Sara Salzano <sarasalzano95@gmail.com>
2025-11-07 15:27:05 +0100

    Aggiungi route layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import Homepage from "./pages/Homepage";
+import Layout from "./Layouts/Layout";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route element={<Layout />} >
         <Route path="/" element={<Homepage />} />
         <Route path="/viaggi/roma" element={<h1 className="text-center mt-5"></h1>} />
         <Route path="/viaggi/parigi" element={<h1 className="text-center mt-5"></h1>} />
         <Route path="/viaggi/tokyo" element={<h1 className="text-center mt-5"></h1>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
